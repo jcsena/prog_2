0ce0dc2f40a574c62835f42936c53ace28db2a31
Julio Sena <julio.657.js@gmail.com>
2016-04-08 21:06:34 -0500

    change example 6

--- empleado/src/Ejemplo6.java
+++ empleado/src/Ejemplo6.java
@@ -17,5 +17,22 @@ public class Ejemplo6 {
         bc = new Button("B5");
 
     }
+    // proyecto final
+    //centro panel de entorno grafico , norte un nombre cualquira, sur tres botones
+
+    public void launchFrame(){
+        f.add(bn,  BorderLayout.NORTH);
+        f.add(bs,  BorderLayout.SOUTH);
+        f.add(bw,  BorderLayout.WEST);
+        f.add(be,  BorderLayout.EAST);
+        f.add(bc,  BorderLayout.CENTER);
+        f.setSize(200, 200);
+        f.setVisible(true);
+    }
+
+    public static void main (String args[]){
+        Ejemplo6 guiWindow2 = new Ejemplo6();
+        guiWindow2.launchFrame();
+    }
 }
 
