593dd25d0163f40f8ad0482ea9f10210e034a322
Julio Sena <julio.657.js@gmail.com>
2016-04-08 21:12:08 -0500

    add example 7

---
+++ empleado/src/Ejemplo7.java
@@ -0,0 +1,20 @@
+/**
+ * Created by Julio on 8/4/16.
+ */
+import  java.awt.*;
+
+public class Ejemplo7 {
+    private Frame f;
+    private Button b1, b2, b3,b4,b5,b6;
+
+    public Ejemplo7(){
+        f = new Frame("Grid Example");
+        b1 = new Button("1");
+        b2 = new Button("2");
+        b3 = new Button("3");
+        b4 = new Button("4");
+        b5 = new Button("5");
+        b6 = new Button("6");
+    }
+
+}
