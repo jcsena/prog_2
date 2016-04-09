24b4a5b5315ba122f57ba339937c5440ab4060fd
Julio Sena <julio.657.js@gmail.com>
2016-04-08 20:58:09 -0500

    add exmaple 6

---
+++ empleado/src/Ejemplo6.java
@@ -0,0 +1,21 @@
+/**
+ * Created by Julio on 8/4/16.
+ */
+
+import java.awt.*;
+
+public class Ejemplo6 {
+    private Frame f;
+    private Button bn, bs, bw, be, bc;
+
+    public Ejemplo6(){
+        f = new Frame("Borber Layaut");
+        bn = new Button("B1");
+        bs = new Button("B2");
+        bw = new Button("B3");
+        be = new Button("B4");
+        bc = new Button("B5");
+
+    }
+}
+
